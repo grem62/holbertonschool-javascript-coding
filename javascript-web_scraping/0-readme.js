1b95c298ceaff085edaead0daef37d8c2a0a1446
grem62 <gremontmatheo22@gmail.com>
2023-09-22 17:20:35 +0200

    correction

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,14 +1,11 @@
-#!/usr/local/bin/node
+#!/usr/bin/node
 
-const { error } = require('console');
 const fs = require('fs');
 
-const path = process.argv[2];
-
-fs.readFileSync(path, 'utf-8', function(err, data){
-if(err){
-    console.error(`error: ${err}`)
-} else {
-    console.log(data)
-    }
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+  if (err) {
+    console.error(`error: ${err}`);
+  } else {
+    console.log(data);
+  }
 });
